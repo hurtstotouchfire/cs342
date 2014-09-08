9cf3b8e4de4f68d70663bc753472fd8e84193d44
hurtstotouchfire <hurtstotouchfire@gmail.com>
2014-09-08 11:57:53 -0400

    experimenting with io methods

--- hw/src/GuessNumber.java
+++ hw/src/GuessNumber.java
@@ -1,7 +1,10 @@
 public class GuessNumber {
     public static void main (String[] args) {
 	GuessNumber gn = new GuessNumber();
-	//	gn.start_game();
+	gn.start_game();
+    }
+    private void start_game() {
+    	    //method stub
     }
 
 }
---
+++ hw/src/Type.java
@@ -0,0 +1,10 @@
+// Type.java
+class Type
+{
+   public static void main (String [] args) throws java.io.IOException
+   {
+      int ch;
+      while ((ch = System.in.read ()) != -1)
+         System.out.print ((char) ch);
+   }
+}
