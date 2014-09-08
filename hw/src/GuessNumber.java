d648d168dcd8ef076caa7ca1292dc75a7dcc2e21
hurtstotouchfire <hurtstotouchfire@gmail.com>
2014-09-08 14:21:59 -0400

    starting to look at taking input

--- hw/src/GuessNumber.java
+++ hw/src/GuessNumber.java
@@ -2,21 +2,44 @@ public class GuessNumber {
     private int target_num;
     private int range_min = 0;
     private int range_max = 1000;
+    private int guess;
+    private int num_guesses = 0;
 
     public static void main (String[] args) {
 	GuessNumber gn = new GuessNumber();
 	gn.welcome();
 	gn.random_number();
+	gn.take_a_guess();
 	gn.give_up();
     }
     private void welcome() {
-    	    System.out.print ("I'm thinking of a number between " + range_min + " and " + range_max + "...\n");
+	System.out.print ("I'm thinking of a number between " + range_min + " and " + range_max + "...\n");
     }
     private void random_number() {
     // TODO: make this pick a random number between range_min and range_max
-    	    target_num = 50;
+	target_num = 50;
+    }
+    private void take_a_guess() {
+	System.out.println ("Take a guess:");
+
+	// TODO: take input and validate
+
+	num_guesses++;
+	// TODO: compare to num_guesses
+	System.out.println ("You guessed: " + guess);
+	System.out.println ("That makes " + num_guesses + " guesses.");
     }
     private void give_up() {
-    	    System.out.print ("Ok, it's " + target_num + ".\n");
+	System.out.print ("Ok, it's " + target_num + ".\n");
     }
+
+    // put some of this type of stuff in a buffer reader class
+    //	int ch;
+    //	while ((ch = System.in.read()) != '\n') {
+    //	    guess = ((char) ch);
+    //	}
+
+
 }
+
+
