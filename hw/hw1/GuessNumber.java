e61758d4ebd568c74a07ea82823b0e3586c7ae90
hurtstotouchfire <hurtstotouchfire@gmail.com>
2014-09-08 18:02:54 -0400

    take a run at recursive loop for take_a_guess

--- hw/hw1/GuessNumber.java
+++ hw/hw1/GuessNumber.java
@@ -3,50 +3,52 @@ public class GuessNumber {
     private int target_num;
     private int range_min = 0;
     private int range_max = 1000;
-    private int guess;
-    private int num_guesses = 1;
+    private int guess = range_min - 1; //initialize guess outside range
+    private int num_guesses = 0;
 
     public static void main (String[] args) {
 	GuessNumber gn = new GuessNumber();
 	gn.welcome();
 	gn.random_number();
-	gn.take_a_guess();
-	gn.give_up();
+	if (gn.take_a_guess()) 
+	    gn.you_win();
     }
+
     private void welcome() {
 	System.out.print ("I'm thinking of a number between " + range_min + " and " + range_max + "...\n");
     }
+
     private void random_number() {
     // TODO: make this pick a random number between range_min and range_max
 	target_num = 50;
     }
-    private void take_a_guess() {
+
+    private boolean take_a_guess() {
 
 	//TODO: validate inputs
-	// take input and spit it right back out
 	Reader r = new Reader("Take a guess:");
 	try {
 	    guess = r.readln();
-	    System.out.println (guess);
 	} catch (IOException e) {
 	    //care about it
 	}
 
 	num_guesses++;
-	// TODO: compare to num_guesses
 	System.out.println ("You guessed: " + guess);
 	System.out.println ("That makes " + num_guesses + " guesses.");
-    }
-    private void give_up() {
-	System.out.print ("Ok, it's " + target_num + ".\n");
-    }
 
-    // put some of this type of stuff in a buffer reader class
-    //	int ch;
-    //	while ((ch = System.in.read()) != '\n') {
-    //	    guess = ((char) ch);
-    //	}
+	// if base case, return true and win. otherwise recurse.
+	if (guess == target_num)
+	    return true;
+	else {
+	    return take_a_guess();
+	}
 
+    }
+    private void you_win() {
+	System.out.println ("Ok, it's " + target_num + ".");
+	//	System.out.println ("It only took you " + num_guesses + "!");
+    }
 
 }
 
