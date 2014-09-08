03e08c9ed66d78b5bd88bdcb5fee3538aaa5cc58
hurtstotouchfire <hurtstotouchfire@gmail.com>
2014-09-08 12:20:10 -0400

    really easy game has a few methods.

--- hw/src/GuessNumber.java
+++ hw/src/GuessNumber.java
@@ -1,10 +1,22 @@
 public class GuessNumber {
+    private int target_num;
+    private int range_min = 0;
+    private int range_max = 1000;
+
     public static void main (String[] args) {
 	GuessNumber gn = new GuessNumber();
-	gn.start_game();
+	gn.welcome();
+	gn.random_number();
+	gn.give_up();
     }
-    private void start_game() {
-    	    //method stub
+    private void welcome() {
+    	    System.out.print ("I'm thinking of a number between " + range_min + " and " + range_max + "...\n");
     }
-
+    private void random_number() {
+    // TODO: make this pick a random number between range_min and range_max
+    	    target_num = 50;
+    }
+    private void give_up() {
+    	    System.out.print ("Ok, it's " + target_num + ".\n");
+    }
 }
