83ae09d7618e2dbe5b7c1b303237297e331f3712
hurtstotouchfire <hurtstotouchfire@gmail.com>
2014-09-08 20:57:35 -0400

    implemented recursive loop with Scanner

--- hw/hw1/GuessNumber.java
+++ hw/hw1/GuessNumber.java
@@ -1,53 +1,66 @@
-import java.io.IOException;
+import java.util.Scanner;
 public class GuessNumber {
     private int target_num;
-    private int range_min = 0;
+    private int range_min = 1;
     private int range_max = 1000;
-    private int guess;
-    private int num_guesses = 1;
+    private int guess = range_min - 1;
+    private int num_guesses = 0;
 
     public static void main (String[] args) {
 	GuessNumber gn = new GuessNumber();
 	gn.welcome();
 	gn.random_number();
-	gn.take_a_guess();
-	gn.give_up();
+	if (gn.take_a_guess())
+	    gn.you_win();
     }
+
     private void welcome() {
 	System.out.print ("I'm thinking of a number between " + range_min + " and " + range_max + "...\n");
     }
+
     private void random_number() {
     // TODO: make this pick a random number between range_min and range_max
 	target_num = 50;
     }
-    private void take_a_guess() {
 
-	//TODO: validate inputs
-	// take input and spit it right back out
-	Reader r = new Reader("Take a guess:");
-	try {
-	    guess = r.readln();
-	    System.out.println (guess);
-	} catch (IOException e) {
-	    //care about it
+    private boolean take_a_guess() {
+
+	Scanner scant = new Scanner(System.in);
+	System.out.println("Take a guess: ");
+	if (scant.hasNextInt()) {
+	    guess = scant.nextInt();
+	    //System.out.println ("You guessed: " + guess);
+	    //System.out.println ("That makes " + num_guesses + " guesses.");
+	    give_feedback();
+	} else {
+	    System.out.println ("That's not an integer...");
 	}
-
+	
 	num_guesses++;
-	// TODO: compare to num_guesses
-	System.out.println ("You guessed: " + guess);
-	System.out.println ("That makes " + num_guesses + " guesses.");
+	
+	// if base case, return true and win. otherwise recurse.
+	if (guess == target_num)
+	    return true;
+	else {
+	    return take_a_guess();
+	}
+	
     }
-    private void give_up() {
-	System.out.print ("Ok, it's " + target_num + ".\n");
+    
+    private void give_feedback() {
+	if (guess < target_num) {
+	    System.out.println ("That's too low.");
+	} else if (guess > target_num) {
+	    System.out.println ("That's too high.");
+	} else { 
+	    return;
+	}
     }
-
-    // put some of this type of stuff in a buffer reader class
-    //	int ch;
-    //	while ((ch = System.in.read()) != '\n') {
-    //	    guess = ((char) ch);
-    //	}
-
-
+    
+    private void you_win() {
+	System.out.println ("Ok, it's " + target_num + ".");
+	System.out.println ("You got it in " + num_guesses + " guesses!");
+    }   
 }
 
 
