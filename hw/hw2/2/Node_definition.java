8981beee5d2963c0e28d78d4db9e41b2155efb09
Kelly Buchanan <kbuchanan@vistahigherlearning.com>
2014-09-23 17:44:28 -0400

    pulling in Node.java from branch dllist

---
+++ hw/hw2/2/Node_definition.java
@@ -0,0 +1,26 @@
+
+public class Node {
+    private int data;
+    private Node next;
+    private Node prev;
+    
+    public int getData() {
+    return data;
+    }
+    public void setData(int data) {
+    this.data = data;
+    }
+    public Node getNext() {
+    return next;
+    }
+    public void setNext(Node next) {
+    this.next = next;
+    }
+    public Node getPrev() {
+    return prev;
+    }
+    public void setPrev(Node prev) {
+    this.prev = prev;
+    }
+    
+}
