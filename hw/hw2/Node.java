ed7b2b7bb92b5a727c790e4e8ed7c04320cadf60
hurtstotouchfire <hurtstotouchfire@gmail.com>
2014-10-05 14:26:24 -0400

    Revert "HW2 #2 Doubly Linked List"

--- hw/hw2/Node.java
+++ hw/hw2/Node.java
@@ -2,7 +2,6 @@
 public class Node {
     private int data;
     private Node next;
-    private Node prev;
 	
     public int getData() {
 	return data;
@@ -15,12 +14,6 @@ public class Node {
     }
     public void setNext(Node next) {
 	this.next = next;
-    }
-    public Node getPrev() {
-	return prev;
-    }
-    public void setPrev(Node prev) {
-	this.prev = prev;
     }
 	
 }
