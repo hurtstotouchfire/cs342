cf5ac45190fd2865b501ed2d1720c373ded57cdf
hurtstotouchfire <hurtstotouchfire@gmail.com>
2014-10-25 21:43:35 -0400

    move data from Contact to Node

--- hw/pa1/Contact.java
+++ hw/pa1/Contact.java
@@ -4,30 +4,30 @@ import java.io.Serializable;
 public class Contact extends Node implements Serializable {
 
     // Array: [name, email, phoneNumber]
-    private String[] contactInfo;
     
     public void setName(String args) {
-	this.contactInfo[0] = args;
+	this.data[0] = args;
     }
     public String getName() {
-	return this.contactInfo[0];
+	return this.data[0];
     }
 
     public void setEmail(String args) {
-	this.contactInfo[1] = args;
+	this.data[1] = args;
     }
     public String getEmail() {
-	return this.contactInfo[1];
+	return this.data[1];
     }
 
 
     public void setNumber(String args) {
-	this.contactInfo[2] = args;
+	this.data[2] = args;
     }
     public String getNumber() {
-	return this.contactInfo[2];
+	return this.data[2];
     }
-    
+
+
     public String toString() {
 	String rtn = "";
 	// output all fields in a nice string format
--- hw/pa1/Node.java
+++ hw/pa1/Node.java
@@ -2,6 +2,7 @@
 public class Node {
     protected Node next;
     protected Node prev;
+    protected String[] data;
     
     public Node getNext() {
     return next;
@@ -9,11 +10,19 @@ public class Node {
     public void setNext(Node next) {
     this.next = next;
     }
+
     public Node getPrev() {
     return prev;
     }
     public void setPrev(Node prev) {
     this.prev = prev;
+    }
+
+    public String[] getData() {
+    return data;
+    }
+    public void setData(String[] data) {
+    this.data = data;
     }
     
 }
