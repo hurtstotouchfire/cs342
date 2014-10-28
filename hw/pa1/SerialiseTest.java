b2695b5600c80cb575f8529f67178653bbc90a08
hurtstotouchfire <hurtstotouchfire@gmail.com>
2014-10-28 14:22:36 -0400

    Serialization works. Now need to read and write in order.

--- hw/pa1/SerialiseTest.java
+++ hw/pa1/SerialiseTest.java
@@ -10,28 +10,49 @@ public class SerialiseTest {
      * @param args
      */
     public static void main(String[] args) {
-	// TODO Auto-generated method stub
-	TestObject o1 = new TestObject(1);
-	TestObject o2 = new TestObject(2);
-	TestObject o3 = new TestObject(3);
-     
+	//make some contacts
+	String[] contactInfo2 = new String[3];
+	contactInfo2[0] = "Bob";
+	contactInfo2[1] = "kb2@stuff.com"; 
+	contactInfo2[2] = "123-4567";
+	String[] contactInfo3 = new String[3];
+	contactInfo3[0] = "Charlie";
+	contactInfo3[1] = "kb3@stuff.com"; 
+	contactInfo3[2] = "123-4567";
+	String[] contactInfo4 = new String[3];
+	contactInfo4[0] = "Bret";
+	contactInfo4[1] = "kb4@stuff.com"; 
+	contactInfo4[2] = "123-4567";
+
+	Contact o1 = new Contact();
+	o1.setData(contactInfo2);
+	Contact o2 = new Contact();
+	o2.setData(contactInfo3);
+	Contact o3 = new Contact();
+     	o3.setData(contactInfo4);
+
+	System.out.println("here's what goes in:");
+	System.out.println(o1);
+	System.out.println(o2);
+	System.out.println(o3);
+	
+
 	try {
-	    ObjectOutputStream oout = new ObjectOutputStream(
-							     new FileOutputStream("output.bin"));
-               
+	    ObjectOutputStream oout = new ObjectOutputStream(new FileOutputStream("output.bin"));
+	    
 	    oout.writeObject(o1);
 	    oout.writeObject(o2);
 	    oout.writeObject(o3);
 	    oout.flush();
 	    oout.close();
                
-	    ObjectInputStream oin = new ObjectInputStream(
-							  new FileInputStream("output.bin"));
+	    ObjectInputStream oin = new ObjectInputStream(new FileInputStream("output.bin"));
                
 	    Object ob1 = oin.readObject();
 	    Object ob2 = oin.readObject();
 	    Object ob3 = oin.readObject();
                
+	    System.out.println("here's what comes out:");
 	    System.out.println(o1);
 	    System.out.println(o2);
 	    System.out.println(o3);
@@ -41,17 +62,18 @@ public class SerialiseTest {
 	}
     }
 }
-
-class TestObject implements Serializable {
+/*
+class Contact implements Serializable {
     private int foo;
      
-    public TestObject(int i) {
+    public Contact(int i) {
 	foo = i;
     }
      
     public String toString() {
-	return "TestObject::"+foo;
+	return "Contact::"+foo;
     }
      
     public static final long serialVersionUID = 1l;
 }
+*/
