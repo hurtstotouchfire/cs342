69c1cbee26e17ff02f3ec82f34d2b59cb59b00c0
hurtstotouchfire <hurtstotouchfire@gmail.com>
2014-10-28 14:10:23 -0400

    adding serialization example from https://community.oracle.com/message/9051394

---
+++ hw/pa1/SerialiseTest.java
@@ -0,0 +1,57 @@
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+
+public class SerialiseTest {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+	// TODO Auto-generated method stub
+	TestObject o1 = new TestObject(1);
+	TestObject o2 = new TestObject(2);
+	TestObject o3 = new TestObject(3);
+     
+	try {
+	    ObjectOutputStream oout = new ObjectOutputStream(
+							     new FileOutputStream("output.bin"));
+               
+	    oout.writeObject(o1);
+	    oout.writeObject(o2);
+	    oout.writeObject(o3);
+	    oout.flush();
+	    oout.close();
+               
+	    ObjectInputStream oin = new ObjectInputStream(
+							  new FileInputStream("output.bin"));
+               
+	    Object ob1 = oin.readObject();
+	    Object ob2 = oin.readObject();
+	    Object ob3 = oin.readObject();
+               
+	    System.out.println(o1);
+	    System.out.println(o2);
+	    System.out.println(o3);
+               
+	} catch (Exception exc){ 
+	    exc.printStackTrace();
+	}
+    }
+}
+
+class TestObject implements Serializable {
+    private int foo;
+     
+    public TestObject(int i) {
+	foo = i;
+    }
+     
+    public String toString() {
+	return "TestObject::"+foo;
+    }
+     
+    public static final long serialVersionUID = 1l;
+}
